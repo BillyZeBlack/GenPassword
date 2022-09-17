cd47f785b3d038d5efdd335dfdce17818abe29f4
BillyZeBlack <34786459+BillyZeBlack@users.noreply.github.com>
2022-09-17 08:13:32 +0200

    Initial Commit

---
+++ GenPassword/GenPasswordApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenPasswordApp.swift
+//  GenPassword
+//
+//  Created by williams saadi on 17/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct GenPasswordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
